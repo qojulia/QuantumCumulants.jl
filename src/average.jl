5dfc8e7f8d8488fbca457df23765f92d25a4d95f
David Plankensteiner <david.plankensteiner@gmx.at>
2021-03-19 12:24:12 +0100

    Patch mixed order

--- src/average.jl
+++ src/average.jl
@@ -184,7 +184,7 @@ function cumulant_expansion(avg::SymbolicUtils.Term{<:AvgSym},order::Int;simplif
 end
 function cumulant_expansion(avg::SymbolicUtils.Term{<:AvgSym},order::Vector;mix_choice=maximum,kwargs...)
     aon = acts_on(avg)
-    order_ = mix_choice(order[i] for i in aon)
+    order_ = mix_choice(order[get_i(i)] for i in aon)
     return cumulant_expansion(avg,order_;kwargs...)
 end
 cumulant_expansion(x::Number,order;kwargs...) = x
@@ -249,7 +249,7 @@ function check_lhs(lhs,order::Int;kwargs...)
 end
 function check_lhs(lhs,order::Vector;mix_choice=maximum)
     aon = acts_on(lhs)
-    order_ = mix_choice(order[i] for i in aon)
+    order_ = mix_choice(order[get_i(i)] for i in aon)
     (get_order(lhs) > order_) && error("Cannot form cumulant expansion of derivative! Check the left-hand-side of your equations; you may want to use a higher order!")
     return nothing
 end
