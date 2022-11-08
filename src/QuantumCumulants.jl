4ea42607fe4e34db66b4d35ed42851ae8636997d
Christoph Hotter <chr.hotter@gmail.com>
2022-11-08 22:09:57 +0100

    renamed modules

--- src/QuantumCumulants.jl
+++ src/QuantumCumulants.jl
@@ -30,7 +30,7 @@ export HilbertSpace, ProductSpace, ⊗, tensor,
         CorrelationFunction, Spectrum, correlation_u0, correlation_p0,
         ClusterSpace,
         scale,
-        transition_superscript, 
+        transition_superscript,
         Index, reorder, IndexedOperator, IndexedSingleSum, IndexedVariable, DoubleIndexedVariable,
         IndexedDoubleSum, indexed_complete, IndexedCorrelationFunction,
         scaleME, evalME, indexed_complete!, indexed_meanfield, subst_reds, AvgSums, plotME,
@@ -39,7 +39,7 @@ export HilbertSpace, ProductSpace, ⊗, tensor,
 
 const NO_METADATA = SymbolicUtils.NO_METADATA
 
-source_metadata(source, name) = 
+source_metadata(source, name) =
     Base.ImmutableDict{DataType, Any}(Symbolics.VariableSource, (source, name))
 
 include("hilbertspace.jl")
@@ -58,11 +58,11 @@ include("scale.jl")
 include("latexify_recipes.jl")
 include("printing.jl")
 include("indexing.jl")
-include("doubleSums.jl")
-include("averageSums.jl")
-include("indexedMeanfield.jl")
-include("indexedScale.jl")
-include("indexedCorrelation.jl")
+include("index_double_sums.jl")
+include("index_average.jl")
+include("index_meanfield.jl")
+include("index_scale.jl")
+include("index_correlation.jl")
 include("index_utils.jl")
 
 @deprecate heisenberg(args...; kwargs...) meanfield(args...; kwargs...)
