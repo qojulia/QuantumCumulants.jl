77e176b479e13245fb228c4cd3c59ab6bbe6776c
christoph <christoph.hotter@uibk.ac.at>
2024-03-12 16:47:56 +0100

    numeric conversion CollectiveSigma

--- src/utils.jl
+++ src/utils.jl
@@ -326,6 +326,16 @@ end
 
 _to_numeric(op::Destroy, b::QuantumOpticsBase.FockBasis; kwargs...) = QuantumOpticsBase.destroy(b)
 _to_numeric(op::Create, b::QuantumOpticsBase.FockBasis; kwargs...) = QuantumOpticsBase.create(b)
+function _to_numeric(op::CollectiveSigma, b::QuantumOpticsBase.SpinBasis; kwargs...)
+    axis = op.axis
+    if axis == 1 # Sx
+        QuantumOpticsBase.sigmax(b)*0.5
+    elseif axis == 2 # Sy
+        QuantumOpticsBase.sigmay(b)*0.5
+    elseif axis == 3 # Sz
+        QuantumOpticsBase.sigmaz(b)*0.5
+    end
+end
 function _to_numeric(op::Transition, b::QuantumOpticsBase.NLevelBasis; kwargs...)
     i, j = _convert_levels(op; kwargs...)
     return QuantumOpticsBase.transition(b, i, j)
@@ -347,6 +357,7 @@ end
 
 check_basis_match(h, b; kwargs...) = throw(ArgumentError("Hilbert space $h and basis $b are incompatible!"))
 check_basis_match(::FockSpace, ::QuantumOpticsBase.FockBasis; kwargs...) = nothing
+check_basis_match(::CollectiveSpinSpace, ::QuantumOpticsBase.SpinBasis; kwargs...) = nothing
 function check_basis_match(h::NLevelSpace, b::QuantumOpticsBase.NLevelBasis; kwargs...)
     if length(h.levels) != length(b)
         throw(ArgumentError("Hilbert space $h and basis $b have incompatible levels!"))
