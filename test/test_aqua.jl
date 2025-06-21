c76b1bb1bdc153d6393f37cbcb9de5702206d642
Orjan Ameye <orjan.ameye@hotmail.com>
2025-06-21 22:08:20 +0200

    test: add potemtial code quality tests

--- test/test_aqua.jl
+++
@@ -1,9 +0,0 @@
-using QuantumCumulants, Test
-
-@testset "Code Quality" begin
-    using Aqua
-    Aqua.test_piracies(QuantumCumulants, broken = true)
-    Aqua.test_undefined_exports(QuantumCumulants)
-    Aqua.test_project_extras(QuantumCumulants)
-    Aqua.test_stale_deps(QuantumCumulants)
-end
